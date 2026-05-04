e25ad1bb0b02f2f2969b5c9f00b481bcdf24f794
SiRyus1111 <anniefun1111@gmail.com>
2026-05-05 08:37:12 +0900

    26.05.05 사용자 정의 헤더 추가

---
+++ include/Common.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS // 구식 C함수들 경고 끄기
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 구식 윈속함수들 경고 끄기
+
+#include <winsock2.h> // 윈속2 기본헤더
+#include <ws2tcpip.h> // 윈속2 확장헤더
+
+#include <tchar.h> // _T(), ...
+#include <stdio.h> // printf(), ...
+#include <stdlib.h> // exit(), ...
+#include <string.h> // strncpy(), ...
+
+#pragma comment(lib, "ws2_32") // ws2_32.lib 라이브러리 링크 명시
+
+// 에러 출력 후 종료
+inline void err_quit(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char *)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char *)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 에러 출력 후 그대로 유지
+inline void err_display(const char *msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char *)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+// 에러 출력 후 그대로 유지의 에러코드 버전 (WSAStartup() 용)
+inline void err_display(int errcode)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, errcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char *)&lpMsgBuf, 0, NULL);
+	printf("[Error] %s\n", (char *)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
---
+++ include/socketRAII.h
@@ -0,0 +1,169 @@
+﻿#pragma once
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include "NetCommon.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+class WinsockGuard {
+public:
+    WinsockGuard() {
+        WSADATA wsa;
+        int WSAStartupres = WSAStartup(MAKEWORD(2, 2), &wsa);
+        if (WSAStartupres != 0) {
+            std::cerr << "에러 코드 : " << WSAStartupres << '\n';
+            throw std::runtime_error("윈속 초기화 실패");
+        }
+    }
+    ~WinsockGuard() {
+        WSACleanup();
+    }
+};
+
+class ClientSocket {
+private:
+    SOCKET client_sock;
+public:
+    ClientSocket(SOCKET s) : client_sock(s) {}
+
+    ClientSocket(const ClientSocket&) = delete;
+    ClientSocket& operator=(const ClientSocket&) = delete;
+
+    // 이동 생성자
+    ClientSocket(ClientSocket&& other) noexcept : client_sock(other.client_sock) { // noexcept = 이 함수는 예외를 발생시키지 않는다고 컴파일러에게 알려주기. 그래서 이동 최적화.
+        other.client_sock = INVALID_SOCKET;
+    }
+
+    int ClientSockSend(NetState& state, const char* msg, int len) {
+
+        int send_res = send_all(client_sock, state, msg, len);
+        if (send_res == SOCKET_ERROR) {
+            return SOCKET_ERROR;
+        }
+
+        return send_res;
+    }
+
+    int ClientSockRecv(NetState& state, char* buf, int len) {
+
+        int recv_res = recv_all(client_sock, state, buf, len);
+        if (recv_res == SOCKET_ERROR) {
+            return SOCKET_ERROR;
+        }
+        else if (recv_res == 0) {
+            return 0;
+        }
+
+        return recv_res;
+    }
+
+    ~ClientSocket() {
+        if (client_sock != INVALID_SOCKET) {
+            closesocket(client_sock);
+        }
+    }
+};
+
+class ListenSocket {
+private:
+    SOCKET listen_sock;
+
+public:
+    ListenSocket() {
+        listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (listen_sock == INVALID_SOCKET) {
+            err_display("socket()");
+            throw std::runtime_error("socket() 함수 실패");
+        }
+
+        int option = 1;
+        if (setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&option, sizeof(option)) == SOCKET_ERROR) {
+            err_display("setsockopt()");
+        }
+    }
+
+    ListenSocket(const ListenSocket& s) = delete;
+    ListenSocket& operator=(const ListenSocket&) = delete;
+
+    void ListenSockBind(sockaddr_in* addr) {
+        if (bind(listen_sock, (sockaddr*)addr, sizeof(*addr)) == SOCKET_ERROR){
+            err_display("bind()");
+            throw std::runtime_error("bind() 함수 실패");
+        }
+    }
+
+    void ListenSockListen() {
+        if (listen(listen_sock, SOMAXCONN) == SOCKET_ERROR) {
+            err_display("listen()");
+            throw std::runtime_error("listen() 함수 실패");
+        }
+    }
+
+    ClientSocket ListenSockAccept(sockaddr_in* client_addr) {
+        int len = sizeof(*client_addr);
+        SOCKET client_sock = accept(listen_sock, (sockaddr*)client_addr, &len);
+        if (client_sock == INVALID_SOCKET) {
+            err_display("accept()");
+            throw std::runtime_error("accept() 함수 실패");
+        }
+
+        return ClientSocket(client_sock);
+    }
+
+    ~ListenSocket() {
+        if (listen_sock != INVALID_SOCKET) {
+            closesocket(listen_sock);
+        }
+    }
+};
+class ConnectSocket {
+private:
+    SOCKET connect_sock;
+public:
+    ConnectSocket() {
+        connect_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (connect_sock == INVALID_SOCKET) {
+            err_display("socket()");
+            throw std::runtime_error("socket() 함수 실패");
+        }
+    }
+
+    ConnectSocket(const ConnectSocket& s) = delete;
+    ConnectSocket& operator=(const ConnectSocket&) = delete;
+
+    void ConnectSockConnect(sockaddr_in* addr) {
+        if (connect(connect_sock, (sockaddr*)addr, sizeof(*addr)) == SOCKET_ERROR) {
+            err_display("connect()");
+            throw std::runtime_error("connect() 함수 실패");
+        }
+    }
+
+    int ConnectSockSend(NetState& state, const char* msg, int len) {
+        int send_res = send_all(connect_sock, state, msg, len);
+        if (send_res == SOCKET_ERROR) {
+            return SOCKET_ERROR;
+        }
+
+        return send_res;
+    }
+
+    int ConnectSockRecv(NetState& state, char* buf, int len) {
+        int recv_res = recv_all(connect_sock, state, buf, len);
+        if (recv_res == SOCKET_ERROR) {
+            return SOCKET_ERROR;
+        }
+        else if (recv_res == 0) {
+            return 0;
+        }
+
+        return recv_res;
+    }
+
+    ~ConnectSocket() {
+        if (connect_sock != INVALID_SOCKET) {
+            closesocket(connect_sock);
+        }
+    }
+};
